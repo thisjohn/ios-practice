52e4b95438df310139c1fd6a67664d1f37a40244
thisjohn <thisjohn19@gmail.com>
2016-08-05 02:03:06 +0800

    Test ImageView shadow and corner

--- P1/P1/ViewController.m
+++ P1/P1/ViewController.m
@@ -20,8 +20,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 
-    [self tryBasicLayer];
-    //[self tryTransform];
+    //[self tryBasicLayer];
+    [self tryImageViewLayer];
+    //[self tryImageViewTransform];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -35,11 +36,6 @@
     CALayer *layer = [[CALayer alloc] init];
     layer.frame = CGRectMake(50, 100, 200, 200);
 
-    layer.backgroundColor = [UIColor redColor].CGColor;
-
-    //UIImage *image = [UIImage imageNamed:@"Avatar"];
-    //layer.contents = (id)image.CGImage;
-
     layer.borderColor = [UIColor blueColor].CGColor;
     layer.borderWidth = 5;  // included in width
 
@@ -49,12 +45,43 @@
     layer.shadowOffset = CGSizeMake(30, 20);
     layer.shadowOpacity = 0.2;
 
+    layer.backgroundColor = [UIColor redColor].CGColor;
+
+    //UIImage *image = [UIImage imageNamed:@"Avatar"];
+    //layer.contents = (id)image.CGImage;
+
     //layer.masksToBounds = YES;
 
     [self.containerView.layer addSublayer:layer];
 }
 
-- (void) tryTransform {
+- (void) tryImageViewLayer {
+    // Create shadow view on the bottom
+    UIView *shadowView = [[UIView alloc] init];
+    shadowView.frame = CGRectMake(50, 100, 200, 200);
+
+    shadowView.layer.cornerRadius = 10;
+
+    shadowView.layer.shadowColor = [UIColor blackColor].CGColor;
+    shadowView.layer.shadowOffset = CGSizeMake(5, 3);
+    shadowView.layer.shadowOpacity = 0.3;
+
+    shadowView.backgroundColor = [UIColor redColor];
+
+    // Create image view on the top
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Avatar"]];
+    imageView.frame = shadowView.bounds;
+
+    imageView.layer.cornerRadius = shadowView.layer.cornerRadius;
+
+    imageView.layer.masksToBounds = YES; // clip the corner
+
+    // Combine shadowView with imageView
+    [shadowView addSubview:imageView];
+    [self.containerView addSubview:shadowView];
+}
+
+- (void) tryImageViewTransform {
     UIImageView *imageView;
 
     // 1. Scale
@@ -78,5 +105,15 @@
     [imageView.layer setValue:@(M_PI_4) forKeyPath:@"transform.rotation"];
     [self.containerView addSubview:imageView];
 }
+
+/** UIView的圆角+阴影 */
+/*- (void) testTargetView {
+ self.targetView.layer.shadowColor = [UIColor blackColor].CGColor;
+ self.targetView.layer.shadowOffset = CGSizeMake(20, 20);
+ self.targetView.layer.shadowOpacity = 0.5;
+ self.targetView.layer.cornerRadius = 15;
+ 
+ self.targetView.layer.borderWidth = 10;
+ }*/
 
 @end
